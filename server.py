34f2f8848342c6033724318328b5672cfc82d714
chahoefler <charlotte.hfr@gmail.com>
2020-12-07 15:54:44 +0100

    Implémentation Fast Retransmit

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket, time, os, select
-from io import BytesIO
 from numpy import zeros,array
 
 IP = "127.0.0.2"
@@ -12,11 +11,13 @@ MAXLINE = 1024
 buffer_fichier = []
 buffer_segment=[]
 file= "image.jpg"
-
+f_ret_counter =0
 nb_segment = 1
-timeout = 0.00001
+timeout = 0.00040
 j=1
-i=0
+last_last_ack=0
+
+ClientCount=0
 
 #creer le numero du segment sur 6 octets
 
@@ -24,6 +25,8 @@ def init_segment(n):
     u= str(n)
     v= u.zfill(6)
     return v
+
+
 
 #socket creation
 try:
@@ -73,7 +76,7 @@ data=data.decode("ascii")
 data=data[:-1]
 
 
-print("taille data :", len(data), "taille file name:", len(file))
+
 if(data==file) :
 
     print("Client request: %s" % data)
@@ -89,39 +92,56 @@ if(data==file) :
     my_file.close()
     print("la taille de buffer fichier est %d", size)
     last_ack=0
-
+    seg_tot=int(size/1024)
     #file sending
+    temps_avant= time.time()
+    while last_ack != seg_tot:
 
-    while last_ack != int(size/1024):
-
-        if  nb_segment<int(size/1024)+1:
+        if  nb_segment<seg_tot+1:
             buffer_segment = init_segment(nb_segment).encode("Utf8")
             socket_transfer.sendto((buffer_segment+buffer_fichier[nb_segment-1]), (addr))
-            print("Sent segment:", nb_segment)
             nb_segment += 1
 
-        ready = select.select([socket_transfer], [], [], 0)
+        ready = select.select([socket_transfer], [], [], timeout)
         if ready[0]:
-            #print("ready")
-            data, addr = socket_transfer.recvfrom(1024)
-            data=data.decode("Utf8")
-            seg_ack = data[-3:].replace('\x00','')
-            seg_ack=int(seg_ack)
-            last_ack=seg_ack
-            print(seg_ack ,":" , last_ack)
+            last_ack, addr = socket_transfer.recvfrom(1024)
+            last_ack=last_ack.decode("Utf8")
+            last_ack =int(last_ack[-3:].replace('\x00',''))
 
-        if j==5 :
-            if last_ack != nb_segment :
-                nb_segment=last_ack+1
-                print("nb_segment=",nb_segment)
-            j=0
 
-        j=j+1
-    print(int(size/1024))
+
+
+
+
+        if last_last_ack==last_ack :
+            f_ret_counter +=1
+
+            if f_ret_counter==3:
+                #nb_segment=last_ack+1
+                buffer_segment = init_segment(last_ack+1).encode("Utf8")
+                socket_transfer.sendto((buffer_segment+buffer_fichier[last_ack]), (addr))
+                ready = select.select([socket_transfer], [], [], timeout)
+                if ready[0]:
+                    last_ack, addr = socket_transfer.recvfrom(1024)
+                    last_ack=last_ack.decode("Utf8")
+                    last_ack =int(last_ack[-3:].replace('\x00',''))
+                f_ret_counter=0
+
+
+
+        #if j==5 :
+            #if last_ack != nb_segment :
+                #nb_segment=last_ack+1
+
+            #j=0
+
+
+        #j=j+1
+        last_last_ack=last_ack
+
     socket_transfer.sendto(END.encode("Utf8"), addr)
     temps_apres=time.time()
     debit = (size/ (temps_apres-temps_avant))/1000
     print("Débit : ", debit , "ko/s")
     print("File of %d bytes send" %  os.path.getsize("image.jpg"))
-    print(size)
     print("nb of ack received :" , last_ack)
