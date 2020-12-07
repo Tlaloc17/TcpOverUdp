5f5d8b0a6d3a52dd6541bbf284196d61b7464fab
chahoefler <45469605+chahoefler@users.noreply.github.com>
2020-12-07 11:00:27 +0100

    Update server.py

--- server.py
+++ server.py
@@ -11,14 +11,7 @@ END = "FIN"
 MAXLINE = 1024
 buffer_fichier = []
 buffer_segment=[]
-<<<<<<< HEAD
-buffer_backup=[]
-=======
-buffer_ack = bytearray()
-nb_segment = 1
-timeout = 0.5
-seg_manquant=""
->>>>>>> 5a2fee16c534f3388583c4d5ad3c6f265c28c767
+
 
 nb_segment = 1
 timeout = 0.0000001
@@ -84,7 +77,6 @@ print("Client request: %s" % data)
     #open file and put it in a buffer
 my_file = open("image.jpg", "rb")
 size = 0
-
 while size < os.path.getsize("image.jpg"):
     bytes = my_file.read(1024)
     buffer_fichier.append(bytes)
@@ -95,32 +87,20 @@ print("la taille de buffer fichier est %d", size)
 last_ack=0
 
 #file sending
-<<<<<<< HEAD
 
 while last_ack != int(size/1024):
 
     if  nb_segment<int(size/1024)+1:
         buffer_segment = init_segment(nb_segment).encode("Utf8")
         socket_transfer.sendto((buffer_segment+buffer_fichier[nb_segment-1]), (addr))
-        buffer_backup.append(buffer_segment+buffer_fichier[nb_segment-1])
         print("Sent segment:", nb_segment)
         nb_segment += 1
 
     ready = select.select([socket_transfer], [], [], 0)
-=======
-for i in range(int(size/1024)):
-
-    buffer_segment = init_segment(nb_segment).encode("Utf8")
-    nb_segment += 1
-    socket_transfer.sendto((buffer_segment+buffer_fichier[i]), (addr))
-
-    ready = select.select([socket_transfer], [], [], timeout)
->>>>>>> 5a2fee16c534f3388583c4d5ad3c6f265c28c767
     if ready[0]:
         #print("ready")
         data, addr = socket_transfer.recvfrom(1024)
         data=data.decode("Utf8")
-<<<<<<< HEAD
         seg_ack = data[-3:].replace('\x00','')
         seg_ack=int(seg_ack)
         last_ack=seg_ack
@@ -142,25 +122,14 @@ for i in range(int(size/1024)):
 """
 
 """"
-=======
-        seg_manquant = data[-3:].replace("\x00","")
-        seg_manquant= int(seg_manquant)
-
->>>>>>> 5a2fee16c534f3388583c4d5ad3c6f265c28c767
     else:
         print("pas de ack recu, probleme")
         buffer_segment = init_segment(int(seg_manquant+1)).encode("Utf8")
         socket_transfer.sendto((buffer_segment+buffer_fichier[seg_manquant+1]), (addr))
-<<<<<<< HEAD
 """
 
 
 socket_transfer.sendto(END.encode("Utf8"), addr)
-=======
-    #buffer_ack.append(data)
-
-socket_transfer.sendto(END.encode("Utf8"), (addr))
->>>>>>> 5a2fee16c534f3388583c4d5ad3c6f265c28c767
 print("File of %d bytes send" %  os.path.getsize("image.jpg"))
 print(size)
 print("nb of ack received :" , last_ack)
